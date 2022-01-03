00a00f9a5dc6a92e4987d759124687e53c0f3375
Mostafa Pirhayati <mossihayati@gmail.com>
2022-01-03 14:32:54 +0330

    use http again

--- lib/pages/news_page.dart
+++ lib/pages/news_page.dart
@@ -1,38 +1,58 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:mostafa_test/binding/dashboard_binding.dart';
-import 'package:mostafa_test/dio/loading_overlay.dart';
-import 'package:mostafa_test/dio/news_controller.dart';
-import 'package:mostafa_test/dio/news_list_item.dart';
+import 'package:mostafa_test/http/news_model.dart';
+import 'package:mostafa_test/pages_controllers/news_controller.dart';
 
-class NewsPage extends StatelessWidget {
+
+class NewsPage extends GetView<NewsCtrl> {
+
   const NewsPage({Key? key}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('News'),
+        title: const Text('News'),
         backgroundColor: Colors.pinkAccent,
         centerTitle: true,
       ),
-      body:
-      Container(
-        child: GetBuilder<NewsController>(
-          builder: (controller) {
-            return LoadingOverlay(
-              isLoading: controller.isLoading,
-              child: ListView.builder(
-                itemCount: controller.newsList.length,
-                itemBuilder: (context, index) => NewsListItem(
-                  news: controller.newsList[index],
-                ),
-              ),
-            );
+      body: Center(
+        child: FutureBuilder<NewsModel>(
+          future: controller.futureNews,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Text(snapshot.data!.description.toString());
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
+
+            // By default, show a loading spinner.
+            return const CircularProgressIndicator();
           },
         ),
       ),
     );
 
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return MaterialApp(
+  //     title: 'Fetch Data Example',
+  //     theme: ThemeData(
+  //       primarySwatch: Colors.blue,
+  //     ),
+  //     home: Scaffold(
+  //       appBar: AppBar(
+  //         title: const Text('Fetch Data Example'),
+  //       ),
+  //       body:
+  //     ),
+  //   );
+  // }
+
+
 }
--- lib/pages_controllers/news_controller.dart
+++ lib/pages_controllers/news_controller.dart
@@ -1,26 +1,34 @@
-// import 'package:get/get.dart';
-// import 'package:mostafa_test/http/models/api_model_services.dart';
-// import 'package:mostafa_test/http/models/news_model.dart';
-//
-// class NewsCtrl extends GetxController{
-//   final String title = 'News';
-//   // var isLoading = true.obs;
-//   // var newsList = <NewsModel>[].obs;
-//
-//   @override
-//   void onInit() {
-//     // fetchNews();
-//     super.onInit();
-//   }
-//   // fetchNews() async{
-//   //   try{
-//   //     isLoading(true);
-//   //     var news = await ApiServices.fetchNews();
-//   //     if(news != null){
-//   //       newsList.assign(news);
-//   //     }
-//   //   } finally{
-//   //     isLoading(false);
-//   //   }
-//   // }
-// }
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mostafa_test/http/news_model.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+
+class NewsCtrl extends GetxController{
+
+  late Future<NewsModel> futureNews;
+
+  @override
+  Future<void> onInit() async {
+
+    futureNews = fetchNews();
+  }
+
+  Future<NewsModel> fetchNews() async {
+    final response = await http
+        .get(Uri.parse('https://newsapi.org/v1/articles?source=cnn&sortBy=top&apiKey=3309b7e1283f41c7b1d4b5e63c915c33'));
+
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      return NewsModel.fromJson(jsonDecode(response.body));
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load news');
+    }
+  }
+
+
+}
